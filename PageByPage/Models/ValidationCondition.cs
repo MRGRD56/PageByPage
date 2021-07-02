397cd05a48b88a14e165d73f07173b3b796c2212
MRGRD56 <kirgol85@gmail.com>
2021-07-02 14:55:46 +0500

    A lot of changes, still not working

---
+++ PageByPage/Models/ValidationCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageByPage.Models
+{
+    public record ValidationCondition(
+        ValidationPredicate Predicate,
+        string ErrorText,
+        ValidationCallback Callback = null);
+}
